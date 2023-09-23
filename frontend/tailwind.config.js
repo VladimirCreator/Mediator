ea420279df04a6c6c42755af3da024d84a8739b7
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-23 16:27:01 +0500

    Fix Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/**
+  * @type { import('tailwindcss').Config }
+*/
 export default {
     content: [
         './index.html',
-        './src/**/*.{tsx}'
+        './src/**/*.{js,ts,jsx,tsx}'
     ],
     theme: {
         extend: {},
     },
     plugins: [],
 }
-
