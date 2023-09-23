b5d9660a597dbe9616972c03a55c3a3e121f6463
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-24 00:43:37 +0500

    Add `type UIPickerBuilder<T>`

--- frontend/src/Component/Prop/UIPickerProp.ts
+++ frontend/src/Component/Prop/UIPickerProp.ts
@@ -2,13 +2,16 @@ import {
     TabsProps
 } from "@radix-ui/react-tabs";
 
+type UIPickerBuilder<T extends readonly string[]> = {
+    [K in T[number] as `${Lowercase<string & K>}`]: () => JSX.Element;
+}
+
 type UIPickerProp<
     Element,
     Collection extends ReadonlyArray<Element>
 > = {
-    collection: Collection
-    defaultValue: Collection[number]
-
-} & Omit<TabsProps, 'defaultValue'>;
+    collection: Collection;
+    defaultValue: Collection[number];
+} & UIPickerBuilder<Collection> & Omit<TabsProps, 'defaultValue'>;
 
 export default UIPickerProp;
