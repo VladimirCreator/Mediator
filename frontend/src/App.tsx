14b8a145ec9f1adf065250c4656fb0af488999be
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-23 19:10:12 +0500

    `import * as Tabs from '@radix-ui/react-tabs';`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
+import * as Tabs from "@radix-ui/react-tabs";
+import Picker from 'react-mobile-picker';
+
 export default function App() {
+    const [selection, setSelection] = useState(
+        {
+            language: 'Swift'
+        }
+    );
+
     return (
-        <h1>
-            asd
-            </h1>
-    )
+        <>
+            <Picker value={selection} onChange={setSelection}>
+                <Picker.Column name={'A Programming Language'}>
+                    {
+                        ["Swift", "C++", "JavaScript", "TypeScript"].map(
+                            (language) => (
+                                <Picker.Item key={language} value={language}>
+                                    {language}
+                                </Picker.Item>
+                            )
+                        )
+                    }
+                </Picker.Column>
+            </Picker>
+        </>
+    );
 }
