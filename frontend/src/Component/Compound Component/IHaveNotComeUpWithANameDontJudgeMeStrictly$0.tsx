417039f54f637f54333265fd8c1da28342b87227
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-28 00:40:49 +0500

    frontend: Update `/src/Component/Compount Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0.tsx`

--- frontend/src/Component/Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0.tsx
+++ frontend/src/Component/Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0.tsx
@@ -1,8 +1,12 @@
-import Picker from "react-mobile-picker";
-import { Card } from "../Layout/Card";
+import { Root, Trigger } from '@radix-ui/react-tabs'
+import Picker from 'react-mobile-picker'
 
-// Perfectoinism kills me.
-export const IHaveNotComeUpWithANameDontJudgeMeStrictly$0: React.FC = () => {
+import { Card } from '../Layout/Card'
+import { SegmentedControl } from '../Control/SegmentedControl'
+
+/* Variant 1
+*/
+const Variant1: React.FC = () => {
     return (
         <Card component='section'>
             <Picker value={{l: ''}} onChange={() => {}} wheelMode='normal'>
@@ -19,5 +23,29 @@ export const IHaveNotComeUpWithANameDontJudgeMeStrictly$0: React.FC = () => {
                 </Picker.Column>
             </Picker>
         </Card>
-    );
+    )
+}
+
+// Perfectoinism kills me.
+export const IHaveNotComeUpWithANameDontJudgeMeStrictly$0: React.FC = () => {
+    return (
+        <Card component='section'>
+            <Root children={
+                    <SegmentedControl>
+                        {
+                            ['Swift', 'C++'].map(
+                                (language) => (
+                                    <Trigger key={language}
+                                        value={language}
+                                    >
+                                        {language}
+                                    </Trigger>
+                                )
+                            )
+                        }
+                    </SegmentedControl>
+                }
+            />
+        </Card>
+    )
 }
