18b9907ab539765f3dfc2231f6d3b5a62729bca5
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-29 20:41:34 +0500

    backend: Add `/Sources/Bot/Routes.swift`

--- backend/Sources/Bot/Actor/TGBotConnection.swift
+++ backend/Sources/Bot/Actor/TGBotConnection.swift
@@ -5,11 +5,10 @@ internal actor TGBotConnection {
     private var _connection: TGConnectionPrtcl!
 
     internal var connection: TGConnectionPrtcl {
-        get {
-            return _connection
-        }
-        set {
-            _connection = newValue
-        }
+        get { return _connection }
+    }
+
+    internal func setConnection(_ connection: TGConnectionPrtcl) {
+        _connection = connection
     }
 }
--- backend/Sources/Bot/main.swift
+++ backend/Sources/Bot/main.swift
@@ -1,31 +1,21 @@
-import Foundation
 import Vapor
 import TelegramVaporBot
-//
-//fileprivate let token = readToken(from: "COMPILER_BOT_TOKEN")
-//fileprivate let bot: TelegramBot = .init(token: token)
-//
-//fileprivate let router: Router = .init(bot: bot) // Initially Modified: 02:21 PM Sat 23 Sep 2023
-//router["compile"] = { context in
-//    guard let user = context.message?.from else { return false }
-//    context.respondAsync("Hello, \(user.firstName)")
-//
-//    return true
-//}
-//router["sendData"] = { context in
-//    guard let data = context.message?.webAppData else { return false }
-//
-//    print(data)
-//
-//    return true
-//}
-//
-//
-//while let update = bot.nextUpdateSync() {
-//    do {
-//        try router.process(update: update)
-//    }
-//    catch {
-//        print("\(#function)")
-//    }
-//}
+
+/*                                     this ⌄ `try` is actually for `.detect()`.
+*/
+fileprivate let application: Application = try .init(.detect())
+
+fileprivate let connection: TGBotConnection = .init()
+
+fileprivate let bot: TGBot = .init(
+    app: application,
+    // I do not like the naming.
+    botId: Environment.get("COMPILER_BOT_TOKEN") ?? ""
+)
+
+await connection.setConnection(try await TGLongPollingConnection(bot: bot))
+try await connection.connection.start()
+
+try application.run()
+
+defer { application.shutdown() }
