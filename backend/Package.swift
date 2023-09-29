d53d197fc783759c1876d62ebef38a954322f2c2
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-09-29 06:48:46 +0500

    backend: Add `telegram-vapor-bot` as dependency

--- backend/Package.swift
+++ backend/Package.swift
@@ -4,13 +4,15 @@ import PackageDescription
 fileprivate let package: Package = .init(
     name: "Bot",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+        .package(url: "https://github.com/nerzh/telegram-vapor-bot", from: "2.1.0")
     ],
     targets: [
         .executableTarget(
             name: "Bot",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "TelegramVaporBot", package: "telegram-vapor-bot")
             ]
         )
     ]
