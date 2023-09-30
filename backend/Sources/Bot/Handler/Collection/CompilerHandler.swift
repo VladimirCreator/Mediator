cc7cf7161848b9b27aae40dfbfb65a839448a632
Vladimir <thisusernameisalreadybusy@gmail.com>
2023-10-01 03:54:03 +0500

    backend: snapshot

--- backend/Sources/Bot/Handler/Collection/CompilerHandler.swift
+++ backend/Sources/Bot/Handler/Collection/CompilerHandler.swift
@@ -8,7 +8,7 @@ fileprivate struct SwiftWebAppData: Decodable { // Initially Modified: 02:51 AM 
 
     fileprivate let data: Recipe
 
-    fileprivate init(from decoder: Decoder) {
+    fileprivate init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: JSONKeys.self)
 
         guard let data = try? values.decode(Recipe.self, forKey: .data) else {
@@ -19,22 +19,17 @@ fileprivate struct SwiftWebAppData: Decodable { // Initially Modified: 02:51 AM 
     }
 }
 
-internal let compileHandler: TGCommandHandler = .init(commands: ["compile"]) { update, bot in
+internal let compileHandler: TGBaseHandler = .init() { update, bot in
     guard let message = update.message else { return }
-
     guard let data = message.webAppData?.data.data(using: .utf8) else { return }
 
     let decoder: JSONDecoder = .init()
-    guard let recipe = try? decoder.decode(Recipe.self, from: data) else { return }
+    guard let recipe = try? decoder.decode(SwiftWebAppData.self, from: data).data else { return }
 
-    print(recipe.language)
-    print(recipe.text)
-    print(recipe.stdin)
-
-    let cppRunner: ProcessRunner = .init(language: "Swift", fileURLWithPath: "/bin/g++", arguments: ["./recipe.txt", "-o", "./recipe"])
-    let swiftRunner: ProcessRunner = .init(language: "C++", fileURLWithPath: "swiftc", arguments: ["./recipe.txt"])
-    let javascriptRunner: ProcessRunner = .init(language: "JavaScript", fileURLWithPath: "", arguments: ["not implemented"])
-    let typescriptRunner: ProcessRunner = .init(language: "TypeScript", fileURLWithPath: "", arguments: ["not implemented"])
+    let cppRunner: ProcessRunner = .init(language: "C++", fileURLWithPath: "g++", arguments: ["./recipe.cpp", "-o", "./recipe"], ext: "cpp")
+    let swiftRunner: ProcessRunner = .init(language: "Swift", fileURLWithPath: "swiftc", arguments: ["./recipe.swift"], ext: "swift")
+    let javascriptRunner: ProcessRunner = .init(language: "JavaScript", fileURLWithPath: "", arguments: ["not implemented"], ext: "")
+    let typescriptRunner: ProcessRunner = .init(language: "TypeScript", fileURLWithPath: "", arguments: ["not implemented"], ext: "")
 
     cppRunner.nextRunner = swiftRunner
     swiftRunner.nextRunner = javascriptRunner
--- backend/Sources/Bot/IHaveNotComeUpWithAName/ProcessRunner.swift
+++ backend/Sources/Bot/IHaveNotComeUpWithAName/ProcessRunner.swift
@@ -1,30 +1,43 @@
 import Foundation
+import Vapor
 
 internal class ProcessRunner {
     private let language: String
     private let fileURLWithPath: String
     private let arguments: [String]
+    private let ext: String
 
     internal var nextRunner: ProcessRunner?
 
-    internal init(language: String, fileURLWithPath: String, arguments: [String]) {
+    internal init(language: String, fileURLWithPath: String, arguments: [String], ext: String) {
         self.language = language
         self.fileURLWithPath = fileURLWithPath
         self.arguments = arguments
+        self.ext = ext
     }
 
     internal func execute(_ recipe: Recipe) throws -> (stdout: String?, stderr: String?) {
         guard let nextRunner else { return (stdout: nil, stderr: nil) }
+
         guard language == recipe.language else { return try nextRunner.execute(recipe) }
         guard let data = recipe.text.data(using: .utf8) else { return (stdout: nil, stderr: nil) }
 
-        guard FileManager.default.createFile(atPath: "./recipe.txt", contents: data) else { return (stdout: nil, stderr: nil) }
+        guard FileManager.default.createFile(atPath: "./recipe.\(ext)", contents: data) else { return (stdout: nil, stderr: nil) }
 
-        let compileProcess: Process = try .run(
-            URL(fileURLWithPath: fileURLWithPath),
-            arguments: arguments
-        )
-        compileProcess.waitUntilExit()
+        let compileProcess: Process = .init()
+        compileProcess.environment = .init()
+        compileProcess.environment?["PATH"] = Environment.get("PATH")
+        compileProcess.arguments = arguments
+
+        for path in compileProcess.environment!["PATH"]!.split(separator: ":") {
+            compileProcess.executableURL = URL(fileURLWithPath: "\(path)/\(fileURLWithPath)")
+            do {
+                try compileProcess.run(); compileProcess.waitUntilExit()
+            }
+            catch {
+
+            }
+        }
 
         let recipeProcess: Process = .init(); let pipe: Pipe = .init()
         recipeProcess.executableURL = URL(fileURLWithPath: "./recipe")
