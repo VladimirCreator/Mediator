2229230dde2e66a1fd657b1c2d93048bec54c93c
Vladimir <vladimircreator@icloud.com>
2023-10-04 03:20:16 +0500

    backend: Update `StartHandler.swift`

--- backend/Sources/Bot/Telegram/Handler/Collection/StartHandler.swift
+++ backend/Sources/Bot/Telegram/Handler/Collection/StartHandler.swift
@@ -1,17 +1,20 @@
 import TelegramVaporBot
 
 internal let startHandler: TGCommandHandler = .init(commands: ["start"]) { update, bot in
-    // Why does this live breaks Swift?
-    //guard let IHaveNotComeUpWithAName0: String = update.message?.chat.id else { return }
-    guard let IHaveNotComeUpWithAName0 = update.message?.chat.id else { return }
+    guard let id = update.message?.chat.id else { return }
 
-    let IHaveNotComeUpWithAName1: TGSendMessageParams = .init(
-        chatId: .chat(IHaveNotComeUpWithAName0),
-        text: "Мне нужно знать что мне компилировать. Нажмите на кнопку, чтобы дать мне инструкции.",
-        replyMarkup: .replyKeyboardMarkup(
-            .init(keyboard: tgKeyboardButton)
+    try await bot.sendMessage(
+        params: .init(
+            chatId: .chat(id),
+            text: """
+                  Приятно познакомиться!
+                  Даю кнопку, которая при нажатии позволяет указать необходимые атрибуты рецепта. Она находится ниже.
+                  Нажми, чтобы начать!
+                  """
+            ,
+            replyMarkup: .replyKeyboardMarkup(
+                .init(keyboard: tgKeyboardButton, isPersistent: true)
+            )
         )
     )
-
-    try await bot.sendMessage(params: IHaveNotComeUpWithAName1)
 }
