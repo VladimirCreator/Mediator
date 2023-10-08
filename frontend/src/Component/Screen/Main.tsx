dd02d3b4288422e90ed7392cc3d89016c0ed0438
Vladimir <vladimircreator@icloud.com>
2023-10-08 20:15:05 +0500

    frontend: pseudo commit

--- frontend/src/Component/Screen/Main.tsx
+++ frontend/src/Component/Screen/Main.tsx
@@ -1,27 +1,20 @@
 import { Card } from '../Layout/Card'
+import { IHaveNotComeUpWithANameDontJudgeMeStrictly$0 } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0'
+import { IHaveNotComeUpWithANameDontJudgeMeStrictly$1 } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$1'
+import { IHaveNotComeUpWithANameDontJudgeMeStrictly$2 } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$2'
+import { IHaveNotComeUpWithANameDontJudgeMeStrictly$3 } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$3'
 
-import {
-    IHaveNotComeUpWithANameDontJudgeMeStrictly$0,
-    type BetaProps as Beta$0Props
-} from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0'
-
-import {
-    IHaveNotComeUpWithANameDontJudgeMeStrictly$1,
-    type BetaProps as Beta$1Props
-} from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$1'
-
-import {
-    IHaveNotComeUpWithANameDontJudgeMeStrictly$2,
-    type BetaProps as Beta$2Props
-} from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$2'
-
-import {
-    IHaveNotComeUpWithANameDontJudgeMeStrictly$3,
-    type BetaProps as Beta$3Props
-} from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$3'
+/* Props
+*/
+import type { BetaProps as Beta$0Props } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$0'
+import type { BetaProps as Beta$1Props } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$1'
+import type { BetaProps as Beta$2Props } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$2'
+import type { BetaProps as Beta$3Props } from '../Compound Component/IHaveNotComeUpWithANameDontJudgeMeStrictly$3'
 
 type BetaProps = Beta$0Props & Beta$1Props & Beta$2Props & Beta$3Props
 
+/* Component
+*/
 export const Main: React.FC<BetaProps> = (props) => {
     const {
         onSelectLanguage,
