6ce55290a03ed0002d51b0e7aa213aadd0673006
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-11-17 03:03:43 +0000

    Update vite.config.ts. @frontend
    #vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Mediator',
 	esbuild: { /*jsxInject: 'import React from \'react\''*/},
 	plugins: [react()]
 })
